fe8256409dbd45b0083649d98149b3886037bc4c
mjmw1027 <drumnbass@gmx.at>
2022-09-05 07:51:49 +0000

    Final Changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from translator import english_to_french,french_to_english
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(french_to_english('Bonjour'),'Hello')
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test2(self):
+        self.assertEqual(english_to_french('Hello'),'Bonjour')
+
+
+unittest.main()
--- final_project/server.py
+++ final_project/server.py
@@ -5,20 +5,20 @@ import json
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+def english_to_french():
+    translation=language_translator.translate(text=english_text,model_id='en-fr').get_result()
+    french_text= translation['translations'][0]['translation']
+    return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    translation = language_translator.translate(text=french_text,model_id='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
